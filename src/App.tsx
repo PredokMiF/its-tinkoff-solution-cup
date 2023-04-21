a8cc2ab4485916b323b26eeec87c6538caf9677e
Konstantin Grigorchuk <kgrigorchuk@i-novus.ru>
2023-04-21 05:23:28 +0300

    Make sure eslint work

--- src/App.tsx
+++ src/App.tsx
@@ -15,10 +15,10 @@ function App() {
             </div>
             <h1>Vite + React</h1>
             <div className="card">
-                <button type="button" onClick={() => setCount((count) => count + 1)} >
+                <button type="button" onClick={() => setCount((count) => count + 1)}>
                     count is {count}
                 </button>
-                <p >
+                <p>
                     Edit <code>src/App.tsx</code> and save to test HMR
                 </p>
             </div>
